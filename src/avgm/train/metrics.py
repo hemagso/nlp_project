8ccca198d7bdf6d600b55c81d014d0d68a25cfc9
hemagso <hemagso@gmail.com>
2020-04-29 10:37:13 -0400

    Added flag to identify whether inputs are scores or classes. Added type hints and docstrings.

--- src/avgm/train/metrics.py
+++ src/avgm/train/metrics.py
@@ -2,6 +2,8 @@ from collections import defaultdict
 import numpy as np
 from abc import ABC, abstractmethod
 import csv
+from typing import Optional, Tuple
+import torch
 
 
 class Metric(ABC):
@@ -21,8 +23,19 @@ class Metric(ABC):
 
 
 class MultiClassMetrics(Metric):
-    def __init__(self, size):
+    """ This class computes accuracy, precision, recall and confusion matrices for classification.
+    It does that by keeping track of the confusion matrix, and only calculating the actual metrics when
+    necessary.
+    """
+    def __init__(self, size: int, input_type: Optional[str] = "scores"):
+        """ Creates a new Multi Class Classification  metrics
+
+        :param size: Number of classes
+        :param input_type: Whether inputs are Scores (Either raw or probabilities) or the actual classes
+        """
+        assert input_type in ("scores", "classes")
         super().__init__()
+        self.input_type = input_type
         self.size = size
         self.data = defaultdict(  # Epoch dictionary
             lambda: defaultdict(  # Mode dictionary
@@ -31,13 +44,27 @@ class MultiClassMetrics(Metric):
         )
         self.name = "acc"
 
-    def update(self, y_hat, y):
-        y_class = y_hat.argmax(axis=1)
-        for pred, actual in zip(y_class, y):
+    def update(self, y_hat: torch.Tensor, y: torch.Tensor) -> None:
+        """ Update the metrics with a new batch of observations
+
+        :param y_hat: Predicted values for the class. If they are scores they will be transformed into classes
+            by using argmax.
+        :param y: Ground truth classes
+        """
+        if self.input_type == "scores":
+            y_hat = y_hat.argmax(axis=1)
+        for pred, actual in zip(y_hat, y):
             pred, actual = pred.item(), actual.item()
             self.data[self.current_epoch][self.current_mode][pred, actual] += 1
 
-    def calculate(self, epoch=None, mode=None):
+    def calculate(self, epoch: Optional[int] = None, mode: Optional[str] = None) -> Tuple[float, float, float]:
+        """ Calculate the additional metrics.
+
+        :param epoch: Epoch that we want the metrics calculated. If not provided the current one will
+            be used (Optional).
+        :param mode: Mode that we want the metrics calculated. If not provided the current one will be used (Optional)
+        :return: accuracy, precision, recall float tuple.
+        """
         epoch = epoch if epoch is not None else self.current_epoch
         mode = mode if mode is not None else self.current_mode
 
@@ -53,7 +80,12 @@ class MultiClassMetrics(Metric):
 
         return accuracy, precision, recall
 
-    def to_csv(self, filepath):
+    def to_csv(self, filepath: str) -> None:
+        """ Save metrics to a CSV file. All epochs and modes will be saved. Only the confusion matrix data will be
+        saved, since other metrics can be calculated from it.
+
+        :param filepath: Path to the file.
+        """
         with open(filepath, "w", encoding="utf-8") as f:
             writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC, lineterminator="\n")
             writer.writerow(("epoch", "mode", "pred", "actual", "count"))
